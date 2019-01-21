cd58de8ed5c77dfef0c35819f81666332f97ec22
iamPres <prestowillis@gmail.com>
2019-01-21 05:54:55 -0800

    added multiple attempts

--- agents/self_evolving_car/train.py
+++ agents/self_evolving_car/train.py
@@ -1,11 +1,12 @@
 import math
-import random
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator, GameInfoState
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from examples.levi.input_formatter import LeviInputFormatter
 from examples.levi.output_formatter import LeviOutputFormatter
-import sys, os
+import time
+import sys
+import os
 
 
 class PythonExample(BaseAgent):
@@ -23,13 +24,17 @@ class PythonExample(BaseAgent):
         self.pop = 10  # population for bot looping
         self.num_best = 5
         self.gen = 0
-        self.pos = 0
+        self.min_distance_to_ball = []
+        self.ball_set = ((0, -1500, 0), (0, 0, 0)), ((1000, 0, 1000), (0, -1500, 0)), \
+                        ((-1000, 0, 1000), (0, -1500, 0)), ((-3000, -3000, 1000), (1500, 0, 0)), \
+                        ((3000, -3000, 1000), (-1500, 0, 0))
+        self.attempt = 0
         self.max_frames = 5000
         self.bot_list = [self.Model() for _ in range(self.pop)]  # list of Individual() objects
         self.bot_list[-self.num_best:] = [self.Model()] * self.num_best  # make sure last bots are the same
         self.bot_fitness = [0] * self.pop
         self.fittest = None  # fittest object
-        self.mut_rate = 0.2  # mutation rate
+        self.mut_rate = 2  # mutation rate
         self.distance_to_ball = [math.inf] * self.max_frames  # set high for easy minimum
         self.input_formatter = LeviInputFormatter(team, index)
         self.output_formatter = LeviOutputFormatter(index)
@@ -46,20 +51,23 @@ class PythonExample(BaseAgent):
         distance_to_ball_x = packet.game_ball.physics.location.x - my_car.physics.location.x
         distance_to_ball_y = packet.game_ball.physics.location.y - my_car.physics.location.y
         distance_to_ball_z = packet.game_ball.physics.location.z - my_car.physics.location.z
-        self.distance_to_ball[self.frame] = math.sqrt(distance_to_ball_x**2 + distance_to_ball_y**2 + distance_to_ball_z**2)
+        self.distance_to_ball[self.frame] = math.sqrt(
+            distance_to_ball_x ** 2 + distance_to_ball_y ** 2 + distance_to_ball_z ** 2)
 
         # RENDER RESULTS
-        action_display = "GEN: "+str(self.gen+1)+" | BOT: "+str(self.brain)#+" \nTURN: "+str(self.botList[self.brain].Nodes[1].node(self.distance_to_ball_x,self.distance_to_ball_y,self.distance_to_ball_z,4,5,6,7))+" \nBOOST: "+str(self.botList[self.brain].Nodes[2].node(self.distance_to_ball_x,self.distance_to_ball_y,self.distance_to_ball_z,8,9,10,11))+" \nTHROTTLE: "+str(self.botList[self.brain].Nodes(self.distance_to_ball_x,self.distance_to_ball_y,self.distance_to_ball_z,0,1,2,3))
+        action_display = "GEN: " + str(self.gen + 1) + " | BOT: " + str(self.brain)
 
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
+        draw_debug(self.renderer, action_display)
 
-        # # STOP EVOLVING WHEN THE BALL IS TOUCHED
-        # if packet.game_ball.latest_touch.player_name == "Self-Evolving-Car":
-        #     self.mut_rate = 0
+        # STOP EVOLVING WHEN THE BALL IS TOUCHED
+        if packet.game_ball.latest_touch.player_name == "Self-Evolving-Car":
+            self.mut_rate = 0
 
         # GAME STATE
         car_state = CarState(boost_amount=100)
-        ball_state = BallState(Physics(velocity=Vector3(0, 0, 0), location=Vector3(0, -1000, 1200), angular_velocity=Vector3(0, 0, 0)))
+        velocity = self.ball_set[self.attempt][1]
+        ball_state = BallState(
+            Physics(velocity=Vector3(velocity[0], velocity[1], velocity[2]), location=Vector3(z=1000)))
         game_state = GameState(ball=ball_state, cars={self.index: car_state})
         self.set_game_state(game_state)
 
@@ -69,30 +77,39 @@ class PythonExample(BaseAgent):
         self.controller_state = self.output_formatter.format_model_output(outputs, [packet])[0]
 
         # KILL
-        if (my_car.physics.location.z < 100 or my_car.physics.location.z > 1950 or my_car.physics.location.x < -4000 or my_car.physics.location.x > 4000 or my_car.physics.location.y > 5000) and self.frame > 50:
-            #self.botList[self.brain].fitness = (1/self.frame+1)*100000
+        if (my_car.physics.location.z < 100 or my_car.physics.location.z > 1950 or my_car.physics.location.x < -4000 or
+            my_car.physics.location.x > 4000 or my_car.physics.location.y > 5000
+            or my_car.physics.location.y < -5000) and self.frame > 50:
             self.frame = self.max_frames
 
         # LOOPS
         self.frame = self.frame + 1
 
         if self.frame >= self.max_frames:
+            self.attempt += 1
             self.frame = 0
-            self.calc_fitness()
+            try:
+                self.reset()  # reset at start
+            except:
+                pass
+
+        if self.attempt > 4:
+            self.attempt = 0
+            self.calc_min_fitness()
             self.brain += 1  # change bot every reset
             self.controller_state = SimpleControllerState()  # reset controller
-            self.reset()  # reset at start
+            self.reset()
 
         if self.brain >= self.pop:
             self.gen += 1
             self.brain = 0  # reset bots after all have gone
-
-            self.avg_best_fitness()
+            self.calc_fitness()
+            # self.avg_best_fitness()
             self.calc_fittest()
 
             # PRINT GENERATION INFO
             print("")
-            print("     GEN = "+str(self.gen))
+            print("     GEN = " + str(self.gen))
             print("-------------------------")
             print("FITTEST = BOT " + str(self.fittest))
             print("------FITNESS = " + str(self.bot_fitness[self.fittest]))
@@ -108,14 +125,19 @@ class PythonExample(BaseAgent):
 
         return self.controller_state
 
-    def calc_fitness(self):
-        # CALCULATE MINIMUM DISTANCE TO BALL FOR EACH GENOME
-        min_distance_to_ball = min(self.distance_to_ball)
-        self.bot_fitness[self.brain] = min_distance_to_ball
-
+    def calc_min_fitness(self):
+        # CALCULATE MINIMUM DISTANCE TO BALL FOR EACH ATTEMPT
+        self.min_distance_to_ball[self.attempt] = min(self.distance_to_ball)
         self.distance_to_ball = [math.inf] * self.max_frames
 
-        return min_distance_to_ball
+    def calc_fitness(self):
+        # CALCULATE AVERAGE OF MINIMUM DISTANCE TO BALL FOR EACH GENOME
+        sum1 = sum(self.min_distance_to_ball)
+        sum1 /= len(self.min_distance_to_ball)
+
+        self.bot_fitness[self.brain] = sum1
+
+        return sum1
 
     def avg_best_fitness(self):
         # CALCULATE AVG FITNESS OF 5 FITTEST (IDENTICAL) GENOMES
@@ -130,14 +152,16 @@ class PythonExample(BaseAgent):
         return self.fittest
 
     def reset(self):
+        pos = self.ball_set[self.attempt][0]
         # RESET TRAINING ATTRIBUTES AFTER EACH GENOME
-        ball_state = BallState(Physics(velocity=Vector3(0, 0, 0), location=Vector3(self.pos, 5000, 3000),
-                                       angular_velocity=Vector3(0, 0, 0)))
+        ball_state = BallState(Physics(location=Vector3(pos[0], pos[1], pos[2])))
         car_state = CarState(jumped=False, double_jumped=False, boost_amount=33,
-                             physics=Physics(velocity=Vector3(0,0,0),rotation=Rotator(45, 90, 0),location=Vector3(0.0, -4608,500),angular_velocity=Vector3(0, 0, 0)))
+                             physics=Physics(rotation=Rotator(0, 8, 0), velocity=Vector3(0, 0, 0),
+                                             angular_velocity=Vector3(0, 0, 0), location=Vector3(0.0, -4608, 500)))
         game_info_state = GameInfoState(game_speed=1)
         game_state = GameState(ball=ball_state, cars={self.index: car_state}, game_info=game_info_state)
         self.set_game_state(game_state)
+        time.sleep(0.1)
 
     def selection(self):
         # COPY FITTEST WEIGHTS TO ALL GENOMES
@@ -154,10 +178,7 @@ class PythonExample(BaseAgent):
                 param.data[mask] = param_new.data[mask]
 
 
-def draw_debug(renderer, car, ball, action_display):
+def draw_debug(renderer, action_display):
     renderer.begin_rendering()
-    # draw a line from the car to the ball
-    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
-    # print the action that the bot is taking
-    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
+    renderer.draw_string_2d(10, 10, 4, 4, action_display, color=renderer.white())
     renderer.end_rendering()

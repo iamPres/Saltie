d9c413f5ec2d507a06affb5f00d6d57e52229516
iamPres <prestowillis@gmail.com>
2019-05-10 15:46:19 -0400

    references-and-save

--- training/genetic_trainer.py
+++ training/genetic_trainer.py
@@ -22,6 +22,7 @@ import sys
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, path)  # this is for first process imports
 
+from framework.self_evolving_car.genetic_algorithm import GeneticAlgorithm
 from training.linkuru_playlist import make_default_playlist
 from examples.levi.torch_model import SymmetricModel
 
@@ -36,60 +37,16 @@ def create_on_briefing(send_model: Module) -> Callable:
         return None
     return on_briefing
 
-def crossover(parent1, list):
-    """ create new child with some weights from both parents
-    :param parent1 is a parent
-    :param list is the list to duplicate upon
-    :returns new parameters from both parents"""
-
-    state_dict = parent1.state_dict()
-    for bot in list:
-        bot.load_state_dict(state_dict)
-
-        """param_new = []
-        for param1, param2 in zip(parent1.parameters(), parent2.parameters()):
-            param_new = self.torch.rand(param1.data.size())
-            for index, value in enumerate(param_new):
-                if index <= 4:
-                    param_new.data[index] = param1.data[index]
-                else:
-                    param_new.data[index] = param2.data[index]
-
-        return param_new.data"""
-
-def mutate(list, mut_rate):
-    """Randomizes a certain amount of the first five models' parameters based on mutation rate
-    :param list contains the parameters to be mutated
-    :param mut_rate is the mutation rate"""
-
-    for i, bot in enumerate(list):
-        new_genes = SymmetricModel()
-        for param, param_new in zip(bot.parameters(), new_genes.parameters()):
-            mask = torch.rand(param.data.size()) < mut_rate / (i + 1)
-            param.data[mask] = param_new.data[mask]
-
-def calc_fittest(list):
-    """calculates the fittest bot of each generation
-    :param list is input array to find minimum
-    :returns tuple of the fittest score and index """
-
-    index = 0
-    temp = math.inf
-    for count, fitness in enumerate(list):
-        if fitness < temp:
-            temp = fitness
-            index = count
-    return index
-
 if __name__ == '__main__':
     logger = get_logger('genetic algorithm')
-
+    ga = GeneticAlgorithm()
     gen = 0 # Init generation count
     bot_count = 10
     fitness = [0]*bot_count # Init fitness matrix
     fittest = math.inf
     models = [SymmetricModel().share_memory() for _ in range(bot_count)]
     mut_rate = 0.2
+    num_fittest = 5
 
     with setup_manager_context() as setup_manager:
         while True:
@@ -103,11 +60,15 @@ if __name__ == '__main__':
                 result = next(run_playlist(playlist, setup_manager=setup_manager)) # Get result
                 fitness[bot] = result.grade.loss # Assign Fitness
                 logger.info("BOT: "+str(bot)+" "+str(fitness[bot]))#+str(models[bot].state_dict())) # Log Info
-                fittest = calc_fittest(fitness) # Calculate fittest bot
+                fittest = ga.calc_fittest(fitness) # Calculate fittest bot
 
                 bot += 1
 
-            #crossover(models[fittest], models)
-            #mutate(models, mut_rate)
+                if isinstance(result.grade, Pass):
+                    torch.save(model.state_dict(), f'exercise_{result.reproduction_info.playlist_index}.mdl')
+                    break
+
+            ga.crossover(models[fittest], models)
+            ga.mutate(models[:num_fittest], mut_rate)
 
             print("GENERATION: "+str(gen)+" | "+"FITTEST: "+str(fittest))
